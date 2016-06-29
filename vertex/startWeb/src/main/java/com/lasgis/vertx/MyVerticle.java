df88d8352fda95cf0cf7834dec48e47551f72cb3
LasGIS <laskin@yandex.ru>
2016-06-29 21:48:27 +0700

    start.web::начало

---
+++ vertex/startWeb/src/main/java/com/lasgis/vertx/MyVerticle.java
@@ -0,0 +1,43 @@
+package com.lasgis.vertx;
+
+import io.vertx.core.AbstractVerticle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+This is a simple Java verticle which receives `ping` messages on the event bus and sends back `pong` replies
+ */
+public class MyVerticle extends AbstractVerticle {
+
+    static final Logger LOG = LoggerFactory.getLogger(MyVerticle.class);
+
+    public MyVerticle() {
+        LOG.info("-conf myconf.json");
+    }
+
+    @Override
+    public void start() {
+
+        System.out.println("start MyVerticle");
+        //vertx.createHttpServer()
+/*
+        vertx.eventBus().registerHandler(
+            "ping-address", new Handler<Message<String>>() {
+                @Override
+                public void handle(Message<String> message) {
+                    message.reply("pong!");
+                    container.logger().info("Sent back pong");
+                }
+            }
+        );
+
+        container.logger().info("PingVerticle started");
+
+*/
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("stop MyVerticle");
+    }
+}
