f2ba023eff5df60d1deef77d42d2fea9314145aa
LasGIS <laskin@yandex.ru>
2017-02-17 17:08:43 +0600

    ReactJSX:continue developing

--- ReactJSXStart/src/main/java/lasgis/react/start/controller/AjaxController.java
+++ ReactJSXStart/src/main/java/lasgis/react/start/controller/AjaxController.java
@@ -23,6 +23,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import lasgis.react.start.model.SimpleRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,32 +36,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @RequestMapping(value = "manager")
+@Slf4j
 public class AjaxController {
-    /*
-Request URL:http://localhost:8099/react/manager/check.do
-Request Method:GET
-Status Code:200 OK
-Remote Address:127.0.0.1:8099
-Response Headers
-view parsed
-HTTP/1.1 200 OK
-Server: Apache-Coyote/1.1
-Content-Type: application/json;charset=UTF-8
-Transfer-Encoding: chunked
-Date: Thu, 16 Feb 2017 13:31:45 GMT
-Request Headers
-view parsed
-GET /react/manager/check.do HTTP/1.1
-Host: localhost:8099
-Connection: keep-alive
-Cache-Control: max-age=0
-Upgrade-Insecure-Requests: 1
-User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,* / *;q=0.8
-Accept-Encoding: gzip, deflate, sdch, br
-Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4
-Cookie: io=mjiV_mEaVlvoLLCOAAAN
-*/
 
     @RequestMapping(value = "check.do", method = {RequestMethod.GET, RequestMethod.POST},
         headers = {
@@ -78,22 +55,13 @@ Cookie: io=mjiV_mEaVlvoLLCOAAAN
         return SimpleRequest.of("state", "info");
     }
 
-    @RequestMapping(value = "check1.do", method = {RequestMethod.GET, RequestMethod.POST}//,
-/*
-        headers = {
-            //"Access-Control-Allow-Origin=http://localhost:*",
-            "Content-Type=application/json;charset=UTF-8",
-            //"Content-Type=text/html,application/xhtml+xml,application/xml;charset=UTF-8",
-            "Accept=text/html,application/xhtml+xml,application/xml;charset=UTF-8"
-        },
-*/
-//        produces = {"text/plain", "application/*"}
-    )
+    @RequestMapping(value = "check1.do", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
     public SimpleRequest checkOnText(
         final HttpServletRequest request, final HttpServletResponse response
     ) throws Exception {
-        response.addHeader("Access-Control-Allow-Origin", "http://localhost:3001");
-        return SimpleRequest.of("state", "info");
+        response.addHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
+        log.info("check1.do with header:origin = {}", request.getHeader("origin"));
+        return SimpleRequest.of("ok", "Вот эта строка должна показаться");
     }
 }
--- ReactJSXStart/src/main/webnode/src/components/MyReactPage/MyReactPage.js
+++ ReactJSXStart/src/main/webnode/src/components/MyReactPage/MyReactPage.js
@@ -13,70 +13,75 @@ const title = 'MyReactPage first blood';
 
 class MyReactPage extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {jsonData: {data: "jsonData"}};
+  constructor(props) {
+    super(props);
+    this.state = {
+      jsonData: {data: "jsonData"},
+      error: null
     };
+  }
 
-    static propTypes = {
-      content: PropTypes.string,
-      title: PropTypes.string,
-      jsonData: PropTypes.oneOfType(PropTypes.string, PropTypes.object),
-    };
+  static propTypes = {
+    content: PropTypes.string,
+    title: PropTypes.string,
+    jsonData: PropTypes.oneOfType(PropTypes.string, PropTypes.object),
+  };
 
-    static contextTypes = {
-      onSetTitle: PropTypes.func.isRequired
-    };
+  static contextTypes = {
+    onSetTitle: PropTypes.func.isRequired
+  };
 
-    componentWillMount() {
-      this.context.onSetTitle(title);
-    };
+  componentWillMount() {
+    this.context.onSetTitle(title);
+  }
 
-    componentDidMount() {
-      this.setState({jsonData : {data: "componentDidMount"}});
-      /*
-       var func = async () => {
-       const query = `http://gturnquist-quoters.cfapps.io/api/random`;
-       const response = await fetch(query);
-       const { datae } = await response.json();
-       console.log('response ', response)
-       };
-       var data = func();
-       console.log('data ', data);
-       */
-      /*
-       fetch('http://gturnquist-quoters.cfapps.io/api/random')
-       .then(function(response) {
-       console.log('response ', response);
-       return response.json()
-       }).then(function(json) {
-       jsonData = json;
-       console.log('parsed json', json);
-       }).catch(function(ex) {
-       console.log('parsing failed', ex);
-       })
-       */
-      axios.get('http://localhost:8099/react/manager/check1.do')
-        .then(response => {
-          console.log('response = ', response);
-          console.log('this = ', this);
-          this.setState({jsonData : response.data});
-        }).catch(error => {
-          console.log('error ', error);
-        });
-      console.log('this = ', this);
-    };
+  componentDidMount() {
+/*
+    (async () => {
+      const query = 'http://vlaskin-2.omsk.luxoft.com:8099/react/manager/check1.do';
+      const response = await fetch(query);
+      const text = await response.json().catch(error => {
+        console.log('error ', error);
+      });
+      this.setState({jsonData: text});
+      console.log('text = "', text, '"; typeOf(text) = ', typeof(text));
+    })();
+*/
+    fetch('http://vlaskin-2.omsk.luxoft.com:8099/react/manager/check1.do')
+      .then( response => {
+        console.log('response ', response);
+        return response.json();
+      }).then( json => {
+        console.log('parsed json', json);
+        this.setState({jsonData: json});
+      }).catch( error => {
+        console.log('parsing failed', error);
+      });
 
-    render() {
-        return (
-          <div className={s.root}>
-            <div className={s.container}>
-              <h1>{title}</h1>
-              <p>{JSON.stringify(this.state.jsonData)}</p>
-            </div>
-          </div>
-        );
-    }
+/*
+    axios.get('http://vlaskin-2.omsk.luxoft.com:8099/react/manager/check1.do')
+      .then(response => {
+        console.log('response = ', response);
+        console.log('this = ', this);
+        this.setState({jsonData: response.data});
+      }).catch(error => {
+        console.log('error ', error);
+      });
+    console.log('this = ', this);
+*/
+  }
+
+  render() {
+    return (
+      <div className={s.root}>
+        <div className={s.container}>
+          <h1>{title}</h1>
+
+          <p>{JSON.stringify(this.state.jsonData)}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withStyles(MyReactPage, s);
