e2052764e7271d0e8466fbbfea20033604ff9cc0
Vladimir Laskin <VLaskin@luxoft.com>
2019-04-03 20:36:28 +0600

    hibernate-check:: развитие

--- hibernate/src/main/java/com/lasgis/hibernate/check/repository/UmRole.java
+++ hibernate/src/main/java/com/lasgis/hibernate/check/repository/UmRole.java
@@ -1,27 +1,29 @@
 package com.lasgis.hibernate.check.repository;
 
+import lombok.Data;
 
+@Data
 public class UmRole {
 
-  private String umrleRoleId;
-  private String umrleDescription;
+    private String umrleRoleId;
+    private String umrleDescription;
 
 
-  public String getUmrleRoleId() {
-    return umrleRoleId;
-  }
+    public String getUmrleRoleId() {
+        return umrleRoleId;
+    }
 
-  public void setUmrleRoleId(String umrleRoleId) {
-    this.umrleRoleId = umrleRoleId;
-  }
+    public void setUmrleRoleId(String umrleRoleId) {
+        this.umrleRoleId = umrleRoleId;
+    }
 
 
-  public String getUmrleDescription() {
-    return umrleDescription;
-  }
+    public String getUmrleDescription() {
+        return umrleDescription;
+    }
 
-  public void setUmrleDescription(String umrleDescription) {
-    this.umrleDescription = umrleDescription;
-  }
+    public void setUmrleDescription(String umrleDescription) {
+        this.umrleDescription = umrleDescription;
+    }
 
 }
--- hibernate/src/main/java/com/lasgis/hibernate/check/repository/UmUserRole.java
+++ hibernate/src/main/java/com/lasgis/hibernate/check/repository/UmUserRole.java
@@ -1,37 +1,12 @@
 package com.lasgis.hibernate.check.repository;
 
+import lombok.Data;
 
+@Data
 public class UmUserRole {
 
-  private long umurlUserRoleId;
-  private long umusrUserId;
-  private String umrleRoleId;
-
-
-  public long getUmurlUserRoleId() {
-    return umurlUserRoleId;
-  }
-
-  public void setUmurlUserRoleId(long umurlUserRoleId) {
-    this.umurlUserRoleId = umurlUserRoleId;
-  }
-
-
-  public long getUmusrUserId() {
-    return umusrUserId;
-  }
-
-  public void setUmusrUserId(long umusrUserId) {
-    this.umusrUserId = umusrUserId;
-  }
-
-
-  public String getUmrleRoleId() {
-    return umrleRoleId;
-  }
-
-  public void setUmrleRoleId(String umrleRoleId) {
-    this.umrleRoleId = umrleRoleId;
-  }
+    private long umurlUserRoleId;
+    private long umusrUserId;
+    private String umrleRoleId;
 
 }
