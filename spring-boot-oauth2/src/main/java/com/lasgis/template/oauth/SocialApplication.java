31f42a3b54a9807454e13fdf5b82d2a572148493
VLaskin <vlaskin@it-one.ru>
2021-03-31 15:24:19 +0600

    OAuth: Add Spring Security OAuth2 Template

---
+++ spring-boot-oauth2/src/main/java/com/lasgis/template/oauth/SocialApplication.java
@@ -0,0 +1,50 @@
+package com.lasgis.template.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("login"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests(a -> a
+                .antMatchers("/", "/error", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            )
+            .csrf(c -> c
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            )
+            .logout(l -> l
+                .logoutSuccessUrl("/").permitAll()
+            )
+            .oauth2Login();
+    }
+
+}
