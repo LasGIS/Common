06cc74ace50422d19f97c4aa172c268f764c0bd8
VLaskin <vlaskin@it-one.ru>
2021-04-04 01:32:05 +0600

    OAuth2: add another react OAuth2

---
+++ spring-boot-oauth/src/main/java/com/lasgis/template/oauth/SocialApplication.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021. LasGIS
+ */
+package com.lasgis.template.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("login"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/*", "/static/**", "/error").permitAll()
+            .anyRequest().authenticated()
+            .and().oauth2Login()
+            .and().exceptionHandling()
+            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            .and().logout().invalidateHttpSession(true).logoutSuccessUrl("/");
+        ;
+    }
+
+}
