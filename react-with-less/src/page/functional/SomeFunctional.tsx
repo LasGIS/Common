b9ee0163e5a20a1ce08c675ae0f4a457235ff195
Vladimir Laskin <vlaskin@it-one.ru>
2023-04-28 15:39:50 +0600

    косметические изменения SomeFunctional.tsx

--- react-with-less/src/page/functional/SomeFunctional.tsx
+++ react-with-less/src/page/functional/SomeFunctional.tsx
@@ -1,8 +1,18 @@
 import { useState } from 'react';
+import { groupByField } from '../test/imTesting';
+import { arr1 } from '../test/data';
+
+const handler = () => {
+  groupByField(arr1, 'country');
+};
 
 function SomeFunctional() {
   const [color] = useState<string>('red');
-  return <div style={{ background: color }}>hhhhh</div>;
+  return (
+    <div style={{ background: color }} onClick={handler}>
+      Click
+    </div>
+  );
 }
 
 export default SomeFunctional;
