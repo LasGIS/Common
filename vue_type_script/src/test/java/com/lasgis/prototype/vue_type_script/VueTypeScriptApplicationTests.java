1fae90926dd91c092fd16722d6c8633742c7133b
Vladimir Laskin <vlaskin@it-one.ru>
2023-04-29 11:37:25 +0600

    add vue_type_script

---
+++ vue_type_script/src/test/java/com/lasgis/prototype/vue_type_script/VueTypeScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasgis.prototype.vue_type_script;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueTypeScriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
