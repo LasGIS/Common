baee2b7987e26df7dcd1ae1a92b2f7b790dd2fc3
Vladimir Laskin <laskin@yandex.ru>
2023-05-02 17:46:56 +0600

    add Kafka Akka Test Application

---
+++ kafka-akka-test/src/test/java/com/lasgis/test/kafka/kafkaakkatest/KafkaAkkaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasgis.test.kafka.kafkaakkatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAkkaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
