cd666eb82ad6784bc9dd848b29c094bfe8945a88
Vladimir Laskin <vlaskin@it-one.ru>
2023-06-05 13:05:59 +0600

    hibernate: add UserRoleArrayType (template)

---
+++ hibernate/src/main/java/com/lasgis/hibernate/check/dao/entity/UserRoleArrayType.java
@@ -0,0 +1,100 @@
+/*
+ *  @(#)UserRoleArrayType.java  last: 05.06.2023
+ *
+ * Title: LG prototype for hibernate
+ * Description: Program for support Prototype.
+ * Copyright (c) 2023, LasGIS Company. All Rights Reserved.
+ */
+
+package com.lasgis.hibernate.check.dao.entity;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.usertype.UserType;
+
+import java.io.Serializable;
+import java.sql.Array;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * The Class gsfgsdfg definition.
+ *
+ * @author VLaskin
+ * @since 02.06.2023 : 13:10
+ */
+public class UserRoleArrayType implements UserType {
+    @Override
+    public int[] sqlTypes() {
+        return new int[]{
+            Types.ARRAY
+        };
+    }
+
+    @Override
+    public Class<UserRole[]> returnedClass() {
+        return UserRole[].class;
+    }
+
+    @Override
+    public Object nullSafeGet(ResultSet rs, String[] names, SharedSessionContractImplementor session, Object owner)
+        throws HibernateException, SQLException {
+        Array array = rs.getArray(names[0]);
+        if (array != null) {
+            String[] arr = (String[]) array.getArray();
+            return Arrays.stream(arr).map(UserRole::valueOf).toArray();
+        }
+        return null;
+        //return array != null ? array.getArray() : null;
+    }
+
+    @Override
+    public void nullSafeSet(PreparedStatement st, Object value, int index, SharedSessionContractImplementor session)
+        throws HibernateException, SQLException {
+        if (value != null && st != null) {
+            Array array = session.connection().createArrayOf("text", (UserRole[]) value);
+            st.setArray(index, array);
+        } else {
+            st.setNull(index, sqlTypes()[0]);
+        }
+    }
+
+    @Override
+    public boolean equals(Object x, Object y) throws HibernateException {
+        return Objects.equals(x, y);
+    }
+
+    @Override
+    public int hashCode(Object x) throws HibernateException {
+        return x.hashCode();
+    }
+
+    @Override
+    public Object deepCopy(Object value) throws HibernateException {
+        return value;
+    }
+
+    @Override
+    public boolean isMutable() {
+        return false;
+    }
+
+    @Override
+    public Serializable disassemble(Object value) throws HibernateException {
+        return (Serializable) value;
+    }
+
+    @Override
+    public Object assemble(Serializable cached, Object owner) throws HibernateException {
+        return cached;
+    }
+
+    @Override
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return original;
+    }
+}
