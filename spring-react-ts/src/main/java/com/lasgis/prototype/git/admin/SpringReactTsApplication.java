b6c115d54cd185258d90d8ab6a4998d04d341cfa
Vladimir Laskin <vlaskin@it-one.ru>
2023-06-14 15:51:36 +0600

    spring-react-ts: add template

---
+++ spring-react-ts/src/main/java/com/lasgis/prototype/git/admin/SpringReactTsApplication.java
@@ -0,0 +1,13 @@
+package com.lasgis.prototype.git.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactTsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactTsApplication.class, args);
+	}
+
+}
---
+++ spring-react-ts/src/test/java/com/lasgis/prototype/git/admin/SpringReactTsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasgis.prototype.git.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactTsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
