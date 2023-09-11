7a7b19d319fe1818cfc3af451ffc0df90075a33b
Vladimir Laskin <VLaskin@it-one.ru>
2023-09-11 18:14:57 +0600

    reactive: db: correct user_role

--- reactive/database/src/main/resources/db/migration/V2023.08.31.002__insert_users.sql
+++ reactive/database/src/main/resources/db/migration/V2023.08.31.002__insert_users.sql
@@ -1,13 +1,8 @@
-INSERT INTO role (role_id, description) VALUES ('OPERATOR', 'Оператор');
-INSERT INTO role (role_id, description) VALUES ('SUPERVISOR', 'Старший смены');
-INSERT INTO role (role_id, description) VALUES ('CHIEF', 'Начальник');
-INSERT INTO role (role_id, description) VALUES ('ADMIN', 'Администратор');
-
 INSERT INTO "user" (user_id, login, name, password) VALUES (1, 'LasGIS', 'Владимир Ласкин', '123');
 INSERT INTO "user" (user_id, login, name, password) VALUES (2, 'VPupkin', 'Василий Пупкин', '321');
 
-INSERT INTO user_role (user_id, role_id) VALUES (1, 'ADMIN');
-INSERT INTO user_role (user_id, role_id) VALUES (1, 'CHIEF');
-INSERT INTO user_role (user_id, role_id) VALUES (1, 'SUPERVISOR');
-INSERT INTO user_role (user_id, role_id) VALUES (2, 'SUPERVISOR');
-INSERT INTO user_role (user_id, role_id) VALUES (2, 'OPERATOR');
+INSERT INTO user_role (user_id, role) VALUES (1, 'ADMIN');
+INSERT INTO user_role (user_id, role) VALUES (1, 'CHIEF');
+INSERT INTO user_role (user_id, role) VALUES (1, 'SUPERVISOR');
+INSERT INTO user_role (user_id, role) VALUES (2, 'SUPERVISOR');
+INSERT INTO user_role (user_id, role) VALUES (2, 'OPERATOR');
