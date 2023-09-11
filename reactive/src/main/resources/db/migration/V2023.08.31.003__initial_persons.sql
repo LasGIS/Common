276580dedcf7a5fce29b7055eac9cc59591a397a
Vladimir Laskin <VLaskin@it-one.ru>
2023-09-11 12:03:47 +0600

    reactive: SQL: расширил person_relation.type

--- reactive/src/main/resources/db/migration/V2023.08.31.003__initial_persons.sql
+++ reactive/src/main/resources/db/migration/V2023.08.31.003__initial_persons.sql
@@ -3,8 +3,6 @@
 --==============================================================
 CREATE TABLE person (
   person_id   SERIAL NOT NULL,
-  mother_id   INTEGER REFERENCES person (person_id) ON UPDATE RESTRICT ON DELETE CASCADE,
-  father_id   INTEGER REFERENCES person (person_id) ON UPDATE RESTRICT ON DELETE CASCADE,
   first_name  TEXT   NOT NULL,
   last_name   TEXT   NOT NULL,
   middle_name TEXT,
@@ -14,12 +12,10 @@ CREATE TABLE person (
   )),
   CONSTRAINT person_pk PRIMARY KEY (person_id)
 );
-COMMENT ON TABLE person IS 'Таблица персон';
-COMMENT ON COLUMN person.person_id IS 'Уникальный номер персоны';
-COMMENT ON COLUMN person.mother_id IS 'Мать';
-COMMENT ON COLUMN person.father_id IS 'Отец';
-COMMENT ON COLUMN person.first_name IS 'Имя';
-COMMENT ON COLUMN person.last_name IS 'Фамилия';
+COMMENT ON TABLE person              IS 'Таблица персон';
+COMMENT ON COLUMN person.person_id   IS 'Уникальный номер персоны';
+COMMENT ON COLUMN person.first_name  IS 'Имя';
+COMMENT ON COLUMN person.last_name   IS 'Фамилия';
 COMMENT ON COLUMN person.middle_name IS 'Отчество';
 
 --==============================================================
@@ -29,16 +25,18 @@ CREATE TABLE person_relation (
   person_relation_id SERIAL,
   person_id          INTEGER     NOT NULL REFERENCES person (person_id) ON UPDATE RESTRICT ON DELETE CASCADE,
   person_to_id       INTEGER     NOT NULL REFERENCES person (person_id) ON UPDATE RESTRICT ON DELETE CASCADE,
-  "type"               TEXT UNIQUE NOT NULL CHECK ("type" IN (
-    'SPOUSE',          -- супруг (муж или жена)
-    'SIBLING',         -- родной брат или сестра (если отец или мать неизвестны)
-    'RELATIVE',        -- родственник, родственница
-    'COLLEAGUE'        -- коллега
-  )),
+  "type" TEXT    UNIQUE NOT NULL    CHECK (
+    "type" IN ('PARENT',   -- родитель (мать или отец)
+               'CHILD',    -- ребёнок (сын или дочь)
+               'SPOUSE',   -- супруг (муж или жена)
+               'SIBLING',  -- родной брат или сестра (если отец или мать неизвестны)
+               'RELATIVE', -- родственник, родственница
+               'COLLEAGUE' -- коллега
+    )),
   CONSTRAINT person_role_pk PRIMARY KEY (person_relation_id),
   CONSTRAINT person_role_unique UNIQUE (person_id, person_to_id, "type")
 );
-COMMENT ON TABLE person_relation IS 'Таблица связи. Связывает одну персону с другими персонами';
+COMMENT ON TABLE person_relation                     IS 'Таблица связи. Связывает одну персону с другими персонами';
 COMMENT ON COLUMN person_relation.person_relation_id IS 'Уникальный номер связи';
 COMMENT ON COLUMN person_relation.person_id          IS 'Id основной персоны';
 COMMENT ON COLUMN person_relation.person_to_id       IS 'Id связанной персоны';
